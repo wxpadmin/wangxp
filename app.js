b938396af5ffdf475c388562767237d9db50b1ce
wangxipeng <admin@admindeMacBook-Pro.local>
2018-12-23 18:33:12 +0800

    mac

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-	console.log(111)
+	console.log(222)
 	
     // 登录
     wx.login({
